4141762c10246ce7de1305baf243b48db0536fc3
Shahil Singh Yadav <yshahil0623@gmail.com>
2023-06-13 16:42:01 +0530

    + GET welcome completed

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ var escapeHtml = require("escape-html");
 var session = require("express-session");
 const express_session = require("./auth/express_session/express_session.js");
 const cookieParser = require("cookie-parser");
-const login = require('./controllers/account/login.js')
-const signup = require('./controllers/account/signup.js')
+const welcomeRoute = require('./routes/welcomeRoute.js');
+const userRoutes = require('./routes/userRoutes.js');
 
 app.use(cookieParser());
 app.use(express_session); //to use express-session middleware defined in express_session folder
@@ -16,8 +16,8 @@ app.use(express.json()); //convert all incoming requests to json
 app.use(cors({ credentials: true })); //abiding by cors policy
 
 
-app.use("/", signup, login);
+app.use("/", welcomeRoute, userRoutes);
 
 app.listen(port, () => {
     console.log("Server started at port ", port);
-  });
+});
